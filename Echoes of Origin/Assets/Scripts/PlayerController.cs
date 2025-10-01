c1df93c55e0f368ac33d26c035cd789a67d8651f
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2025-10-01 18:50:36 -0400

    Added and spliced sprite character
    Wrote the code for enumerators for player state

--- Echoes of Origin/Assets/Scripts/PlayerController.cs
+++ Echoes of Origin/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D RB;
+
+    public PlayerState State = PlayerState.Idle;
+
 
     private float Speed = 8;
     // Start is called before the first frame update
@@ -13,27 +17,69 @@ public class PlayerController : MonoBehaviour
         
     }
 
+
+    //Enums are a way of identifying what state player is in, enums have to be written out before used as variable - JE
+    public enum PlayerState
+    {
+        None = 0,
+        Idle = 1,
+        Walking = 2,
+        Jumping = 3,
+        Blocking = 4,
+        Stunned = 5
+    }
+
     // Update is called once per frame
+    //Alternative method of player movement rather than transform - JE
     void Update()
     {
-        Vector2 pos = transform.position;
+        Vector2 vel = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.A))
         {
-            pos.x -= Speed * Time.deltaTime;
+            vel.x = -Speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            pos.y -= Speed * Time.deltaTime;
+            vel.y = -Speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            pos.x += Speed * Time.deltaTime;
+            vel.x = Speed;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            pos.y += Speed * Time.deltaTime;
+            vel.y = Speed;
         }
 
-        transform.position = pos;
+        RB.velocity = vel;
+
+        switch (State)
+        {
+            case PlayerState.Idle:
+                {
+                    //Code for idle animation state - JE
+                    break;
+                }
+            case PlayerState.Walking:
+                {
+                    //Code for walking animation - JE
+                    break;
+                }
+            case PlayerState.Jumping:
+                {
+                    //Code for jumping animation - JE
+                    break;
+                }
+            case PlayerState.Blocking:
+                {
+                    //Code for blocking animation - JE
+                    break;
+                }
+            case PlayerState.Stunned: 
+                {
+                    //Code for stunned animation state - JE
+                    break;
+                }
+        }
     }
 }
